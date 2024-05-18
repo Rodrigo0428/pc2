46ab7ccff473ab32514aace6090c0288e362d6ec
Rodrigo0428 <rodrigocarrasco2004@gmail.com>
2024-05-17 20:10:36 -0500

    App banco

--- AppBanco/obj/Release/net8.0/AppBanco.AssemblyInfo.cs
+++ AppBanco/obj/Release/net8.0/AppBanco.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AppBanco")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1807091ba324c732c8658ac3f1d5b61d6ebe636a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f9c6fb0cbdd8ee1eac7a32a24524dcead2c5ebe")]
 [assembly: System.Reflection.AssemblyProductAttribute("AppBanco")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AppBanco")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
