ec9df35c7f93439752b40052861309d167bdeaa9
Rodrigo0428 <rodrigocarrasco2004@gmail.com>
2024-05-23 22:17:33 -0500

    final change

--- AppBanco/obj/Debug/net8.0/AppBanco.AssemblyInfo.cs
+++ AppBanco/obj/Debug/net8.0/AppBanco.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AppBanco")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ebe46221cbce38494733368b5610cfe0144b9184")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8a942339382aebd52e5800989d84e8b7345ffc3")]
 [assembly: System.Reflection.AssemblyProductAttribute("AppBanco")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AppBanco")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
